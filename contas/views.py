34450416608b88d36199909963e6293c836eb95c
Jose <Josebazjunior09@gmail.com>
2021-01-30 01:49:45 -0300

    Renderizando dados

--- contas/views.py
+++ contas/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 def home(request):
-    return render(request, 'contas/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Entregue').count()
+    pending = orders.filter(status='Pendente').count()
+
+    context = {'orders':orders, 'customers':customers,
+    'total_orders':total_orders, 'delivered':delivered,
+    'pending':pending}
+
+    return render(request, 'contas/dashboard.html', context)
 
 def products(request):
-    return render(request, 'contas/products.html')
+    products = Product.objects.all()
+    return render(request, 'contas/products.html', {'products':products})
 
 def customer(request):
     return render(request, 'contas/customer.html')
