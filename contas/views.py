6a12b0a50d7ca14ad108b71464c26e8fae0f312d
Jose <Josebazjunior09@gmail.com>
2021-01-31 02:04:19 -0300

    CRUD

--- contas/views.py
+++ contas/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import OrderForm
 
 def home(request):
     orders = Order.objects.all()
@@ -28,3 +29,37 @@ def customer(request, pk_test):
 
 	context = {'customer':customer, 'orders':orders, 'order_count':order_count}
 	return render(request, 'contas/customer.html',context)
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        # print('Printando POST', request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request, "contas/order_form.html", context)
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request, "contas/order_form.html", context)
+
+def deleteOrder(request,pk):
+    order = Order.objects.get(id=pk)
+    if request.method == "POST":
+        order.delete()
+        return redirect('/')
+        
+    context = {'item': order}
+    return render(request, "contas/delete.html", context)
+
